ed490713ac4a513cdc228041f67ea45469158be4
Lucas Moraes <155003989+hub-Moraes@users.noreply.github.com>
2026-02-18 17:53:52 -0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import Header from '../widgets/Header/Header';
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <nav className="max-w-7xl mx-auto p-4 border-red-500 border-2 flex justify-between items-center">
+          <Header />
+        </nav>
+      </header>
+
+      <main className="max-w-7xl mx-auto border-blue-500 border-2">
+        <section>
+          <div>Conteudo</div>
+        </section>
+      </main>
+
+      <footer className="max-w-7xl mx-auto border-yellow-500 border-2">
+        <div>Rodapé</div>
+      </footer>
+    </>
+  );
+}
---
+++ src/widgets/Header/Header.tsx
@@ -0,0 +1,16 @@
+import Botao from "@/src/components/Botao/Botao";
+import Logo from "@/src/components/Logo/Logo";
+import Menu from "@/src/components/Menu/Menu";
+
+
+export default function Header() {
+  return (
+    <>
+      <Logo />
+
+      <Menu />
+
+      <Botao />
+    </>
+  );
+}
