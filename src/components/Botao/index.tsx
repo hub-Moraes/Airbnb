8781f213f044613cce24d45f7363aeb45a58d45b
Lucas Moraes <lucas.camilo.m@gmail.com>
2026-02-18 21:58:45 -0300

    feat: adição de icone de user no botão 'entrar'

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,9 +1,10 @@
+import { IconUser } from "@tabler/icons-react";
+
 export default function Botao() {
   return (
-    <>
-      <button className="p-2 w-25 border-2 border-gray-200 rounded-full hover:bg-red-400 hover:border-red-400 hover:text-white">
-        Entrar
-      </button>
-    </>
+    <button className="flex items-center justify-center gap-1 p-3 w-25  border-2 border-gray-200 rounded-full hover:bg-red-400 hover:border-red-400 hover:text-white">
+      <IconUser size={18} />
+      <span className="leading-none">Entrar</span>
+    </button>
   );
 }
