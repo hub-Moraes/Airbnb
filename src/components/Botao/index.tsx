d569728b385bdb2ab98195368731ec42e77d44bb
Lucas Moraes <lucas.camilo.m@gmail.com>
2026-02-19 11:52:30 -0300

    feat: mudança de estratégia ultilizando props para reutilização de componente

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,17 @@
-import { IconUser } from "@tabler/icons-react";
-
-export default function Botao() {
-  return (
-    <button className="flex items-center justify-center gap-1 p-3 w-25  border-2 border-gray-200 rounded-full hover:bg-red-400 hover:border-red-400 hover:text-white">
-      <IconUser size={18} />
-      <span className="leading-none">Entrar</span>
-    </button>
-  );
+interface BotaoProps {
+  icone?: React.ReactNode;
+  children: React.ReactNode;
 }
+
+const Botao = (props: BotaoProps) => {
+  return (
+    <>
+      <button className="flex items-center justify-center gap-1 p-3 w-25  border-2 border-gray-200 rounded-full hover:bg-red-400 hover:border-red-400 hover:text-white">
+        <span>{props.icone}</span>
+        <span>{props.children}</span>
+      </button>
+    </>
+  );
+};
+
+export default Botao;
