087bb42578b83d13e96f280bdd857997b47742d5
Lucas Moraes <lucas.camilo.m@gmail.com>
2026-02-26 14:39:12 -0300

    feat: criação da página de detalhes das acomodações

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -7,7 +7,7 @@ interface BotaoProps {
 const Botao = (props: BotaoProps) => {
   return (
     <>
-      <button className="flex items-center justify-center gap-1 p-3 sm:w-25  border-2 border-gray-200 rounded-full hover:bg-red-400 hover:border-red-400 hover:text-white">
+      <button className="flex items-center justify-center gap-1 p-3 bg-white border-2 border-gray-200 rounded-full hover:bg-red-400 hover:border-red-400 hover:text-white">
         <span>{props.icone}</span>
         <span className="hidden sm:block">{props.children}</span>
       </button>
