ba77f17d1b05f55855040dac9a0485d1e63df3a6
Lucas Moraes <lucas.camilo.m@gmail.com>
2026-03-02 10:08:54 -0300

    feat: adição de logotipo na responsividade mobile

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,6 +5,7 @@ export default function Logo() {
   return (
     <Link href="/">
       <Image className="hidden sm:block" src="/img/Airbnb_Logo.svg" alt="logo" width={120} height={40} />
+      <Image className="sm:hidden" src="/img/Airbnb_Logotipo.png" alt="logotipo" width={40} height={40} />
     </Link>
   );
 }
